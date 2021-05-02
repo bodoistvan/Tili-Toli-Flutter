32925949cbf1bb6356ffb4c66430788699d3d77c
bodoistvan <61791386+bodoistvan@users.noreply.github.com>
2021-05-02 11:27:34 +0200

    counter added

--- lib/CanvasWidget.dart
+++ lib/CanvasWidget.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:lu93rr/PrimitiveWrapper.dart';
 import 'package:lu93rr/TiliTile.dart';
 
@@ -13,7 +15,10 @@ class CanvasState extends State<MyCanvas> {
   static int columns = 6;
   static int rows = 6;
 
+  final _canvasColor = Colors.orange;
   var _tileValues = List(columns * rows);
+
+  var _stepCounter = 0;
 
   CanvasState(){
     for (var i = 0; i < columns * rows; i++){
@@ -23,29 +28,75 @@ class CanvasState extends State<MyCanvas> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: columns.toDouble() * 50,
-        height: rows.toDouble() * 50,
-        margin: EdgeInsets.all((5)),
-        color: Colors.orange,
-        child: Column(
-          children: (){
-                var counter = 0;
-                var columnWidgets = new List<Widget>();
-                for ( var i = 0; i < rows; i++) {
-                  var rowWidgets = new List<Widget>();
-                  for (var j = 0; j < columns; j++) {
-                    var myKey = UniqueKey();
-                    var tile = new TiliTile(onTileClicked, this._tileValues[counter]);
-                    rowWidgets.add(tile);
-                    counter++;
-                  }
-                  columnWidgets.add(Row(children:rowWidgets));
-                }
-              return columnWidgets;
-          }(),
-        )
+
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("Tili-Toli"),
+      ),
+      body: Center(
+          child:
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(55),
+                    width: 200,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.redAccent
+                      )
+                    ),
+                    child: Center(
+                      child: Text("$_stepCounter", textScaleFactor: 1.5,),
+                    ),
+                  ),
+                  Container(
+                      width: columns.toDouble() * 50,
+                      height: rows.toDouble() * 50,
+                      color: _canvasColor,
+                      child: Column(
+                        children: (){
+                          var counter = 0;
+                          var columnWidgets = new List<Widget>();
+                          for ( var i = 0; i < rows; i++) {
+                            var rowWidgets = new List<Widget>();
+                            for (var j = 0; j < columns; j++) {
+                              var myKey = UniqueKey();
+                              var tile = new TiliTile(onTileClicked, this._tileValues[counter], this._canvasColor);
+                              rowWidgets.add(tile);
+                              counter++;
+                            }
+                            columnWidgets.add(Row(children:rowWidgets));
+                          }
+                          return columnWidgets;
+                        }(),
+                      )
+                  )
+                ],
+              )
+          ),
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {this.restartGame()},
+        tooltip: 'Restart',
+        backgroundColor: Colors.red,
+        splashColor: Colors.amber,
+        child: Icon(Icons.autorenew),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  restartGame(){
+    print("restart");
+    setState(() {
+      this._tileValues.shuffle();
+      this._stepCounter = 0;
+    });
   }
 
   onTileClicked(num value){
@@ -53,32 +104,27 @@ class CanvasState extends State<MyCanvas> {
       return;
     
     final index = this._tileValues.indexOf(value);
-    print("index: $index");
     //check neigbors
     //top
     if (index > rows -1){
-      print("top");
       final tni = index - columns;
       if (changeValuePosition(index, tni))
         return;
     }
     //bottom
     if (index < columns * rows - rows){
-      print("bottom");
       final bni = index + columns;
       if (changeValuePosition(index, bni))
         return;
     }
     //left
     if (index % columns != 0){
-      print("left");
       final lni = index - 1;
       if (changeValuePosition(index, lni))
       return;
     }
     //right
     if (index % columns != columns-1){
-      print("right");
       final rni = index + 1;
       if (changeValuePosition(index, rni))
       return;
@@ -89,18 +135,16 @@ class CanvasState extends State<MyCanvas> {
   bool changeValuePosition(num index_x, num index_y) {
     num valy = this._tileValues[index_y];
     if (valy != 0){
-      print("nem");
       return false;
     }
-
 
     num valx = this._tileValues[index_x];
 
     setState(() {
+      this._stepCounter++;
       this._tileValues[index_y] = valx;
       this._tileValues[index_x] = valy;
     });
-    print("igen");
     return true;
   }
 }
